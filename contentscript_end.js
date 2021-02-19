ffd4a6227e42c7bd9eddcda644297ef76ddbcb4d
luoxing.klx <kong.luoxing@gmail.com>
2021-02-19 21:48:34 +0800

    custom font

--- contentscript_end.js
+++ contentscript_end.js
@@ -2,14 +2,17 @@
 var scripts = [ 
     "keybindings.js",
     "transparentKeybindings.js",
-    "vimflowy.js"
+    "vimflowy.js",
+    // "custom_font.js"
     ];
+
+var head = (document.head||document.documentElement)
 
 for (var i=0; i < scripts.length; i++) 
 {
     var s = document.createElement('script');
     s.src = chrome.extension.getURL(scripts[i]);
-    (document.head||document.documentElement).appendChild(s);
+    head.appendChild(s);
     s.onload = function() {
         this.parentNode.removeChild(this);
     };
